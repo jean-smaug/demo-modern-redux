8f6e0d3523d70d3e2de09ac233347fcb51740758
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2022-04-01 10:35:50 +0200

    new and old

--- src/modules/user-old/UserList.component.js
+++ src/modules/user-old/UserList.component.js
@@ -5,10 +5,6 @@ import { selectUsers } from "./user.selector";
 
 const UserList = () => {
   const users = useSelector(selectUsers);
-  console.log(
-    "🚀 ~ file: UserList.component.js ~ line 8 ~ UserList ~ users",
-    users
-  );
 
   return users.map((user) => <User key={user.id} user={user} />);
 };
--- src/store.js
+++ src/store.js
@@ -3,10 +3,14 @@ import { todoSlice } from "./modules/todo/todo.slice";
 import { userSlice } from "./modules/user/user.slice";
 import { userReducer } from "./modules/user-old/user.reducer";
 
+const userReducerMap = {
+  new: userSlice.reducer,
+  old: userReducer,
+};
+
 export const store = configureStore({
   reducer: {
-    users:
-      process.env.REACT_APP_VERSION === "new" ? userSlice.reducer : userReducer,
+    users: userReducerMap[process.env.REACT_APP_VERSION],
     todos: todoSlice.reducer,
   },
 });
